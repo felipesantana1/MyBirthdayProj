bc17262b6fdb52a3b49dc7bbb70ed4cd019d9e3d
Felipe <felipeo510.fs@gmail.com>
2017-08-21 13:25:31 -0700

    my birthday project

---
+++ isItMyBirthday.js
@@ -0,0 +1,12 @@
+var tilMyB_Day = 29;
+
+while(tilMyB_Day > 0) {
+
+    console.log(tilMyB_Day-- + " days until my birthday!");
+
+    if(tilMyB_Day === 0) {
+        console.log("♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪ღ♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•");
+    console.log("♪ღ♪░H░A░P░P░Y░ B░I░R░T░H░D░A░Y░░♪ღ♪");
+    console.log("*•♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•«");   
+    }
+}

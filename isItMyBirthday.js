bdad89bddc6006318772dd7bd0507e7b76bb4aed
Felipe <felipeo510.fs@gmail.com>
2017-08-21 15:39:56 -0700

    modified and finished birthday project

--- isItMyBirthday.js
+++ isItMyBirthday.js
@@ -1,12 +1,20 @@
-var tilMyB_Day = 29;
+var tilMyB_Day = 45;
 
-while(tilMyB_Day > 0) {
+while(tilMyB_Day >= 0) {
 
-    console.log(tilMyB_Day-- + " days until my birthday!");
-
+    if(tilMyB_Day >= 30) {
+        console.log(tilMyB_Day + " days until my birthday, so sad! :( ");
+    }
+    if(tilMyB_Day <= 30) {
+        console.log(tilMyB_Day + " days until my birthday, almost there!");
+    }
+    if(tilMyB_Day <= 5) {
+        console.log(tilMyB_Day + " days until my Birthday< I AM READY!! WOOHOO!!")
+    }
     if(tilMyB_Day === 0) {
         console.log("♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪ღ♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•");
     console.log("♪ღ♪░H░A░P░P░Y░ B░I░R░T░H░D░A░Y░░♪ღ♪");
     console.log("*•♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•«");   
     }
+    tilMyB_Day--;
 }
